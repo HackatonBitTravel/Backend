b7cd7cdf460cf253d9346179a53676af141776f3
Godwin-AKAKPO <mouyiwaakakpo@gmail.com>
2025-11-15 18:26:48 +0100

    l

--- app/routes/schedules.py
+++ app/routes/schedules.py
@@ -8,7 +8,7 @@ from app.core.database import get_db
 from app.models.schedule import Schedule
 from app.models.route import Route
 from app.schemas.schedule import ScheduleCreate, ScheduleResponse
-
+from datetime import datetime
 router = APIRouter(prefix="/schedules", tags=["Schedules"])
 
 @router.post("/", response_model=ScheduleResponse, status_code=201)
@@ -30,10 +30,17 @@ def create_schedule(schedule: ScheduleCreate, db: Session = Depends(get_db)):
     return db_schedule
 
 @router.get("/", response_model=List[ScheduleResponse])
-def list_schedules(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    schedules = db.query(Schedule).offset(skip).limit(limit).all()
+def list_schedules(skip: int = 0, limit: int = 50, db: Session = Depends(get_db)):
+    now = datetime.now()
+    schedules = (
+        db.query(Schedule)
+        .filter(Schedule.departure_time >= now)  # Ne garder que les horaires futurs
+        .order_by(Schedule.departure_time)       # Optionnel : trier par date
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
     return schedules
-
 @router.get("/{schedule_id}", response_model=ScheduleResponse)
 def get_schedule(schedule_id: str, db: Session = Depends(get_db)):
     schedule = db.query(Schedule).filter(Schedule.id == schedule_id).first()
